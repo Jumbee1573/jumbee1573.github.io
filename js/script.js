f3446846fbcad53d77c5f0e17608bc7ea57a7286
Jumbee1573 <stasik1999box@gmail.com>
2017-08-26 18:49:38 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+	
+	
+	
+	
