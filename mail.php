ef703eab8370a7ed500c44e2fcd473c24d800aca
Jumbee1573 <stasik1999box@gmail.com>
2017-12-13 12:24:02 +0300

    commit

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "stasik_box99@mail.ru";
+$sitename = "Заявка с сайта";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
