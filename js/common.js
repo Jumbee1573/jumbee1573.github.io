4d626cb468c2a3bd725985eca9249306cd868448
Jumbee1573 <statik1999box@gmail.com>
2018-05-18 22:29:16 +0300

    commit

--- js/common.js
+++ js/common.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function(){
+$(".header__navbar").removeClass("default");
+$(window).scroll(function(){
+  if ($(this).scrollTop()>30) {
+    $(".header__navbar").addClass("default").fadeIn('fast');
+  }
+  else{
+    $(".header__navbar").removeClass("default").fadeIn('fast');
+  };
+});
+});
