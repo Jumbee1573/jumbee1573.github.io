a29023312479fe4749ba88a2348e5fc295d5c3af
Jumbee1573 <statik1999box@gmail.com>
2018-06-16 19:39:45 +0300

    added js script

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// $(document).ready(function() {
-//   var curPos = $(document).scrollTop();
-//   var height = $("body").height();
-//   var scrollTime = 900;
-//   $("body,html").animate({
-//     "scrollTop": height
-//   }, scrollTime);
-// });
+$(document).ready(function() {
+  var curPos = $(document).scrollTop();
+  var height = $("body").height();
+  var scrollTime = 900;
+  $("body,html").animate({
+    "scrollTop": height
+  }, scrollTime);
+});
 
 new WOW().init();
