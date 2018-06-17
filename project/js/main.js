e7922e76246222648c19e136bd82a0e3a0042347
Jumbee1573 <statik1999box@gmail.com>
2018-06-18 01:17:22 +0300

    added eng language

--- project/js/main.js
+++ project/js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+  $('.en').on('click', function() {
+    $('[data-en]').each(function() {
+      $(this).text($(this).attr('data-en'));
+    });
+    $(this).addClass('active');
+    $('.ru').removeClass('active');
+  });
+
+  $('.ru').on('click', function() {
+    $('[data-ru]').each(function() {
+      $(this).text($(this).attr('data-ru'));
+    });
+    $(this).addClass('active');
+    $('.en').removeClass('active');
+  });
+});
+
 new WOW().init();
