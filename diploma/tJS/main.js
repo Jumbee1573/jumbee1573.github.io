ece2c5becccfe06eed5a00598fddb27fd4bb793b
Jumbee1573 <stasik1999box@gmail.com>
2019-01-03 21:12:37 +0300

    version 1.0

---
+++ diploma/tJS/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".header__menu-mobile, .header__menu").on("click", "a", function (event) {
+    // event.preventDefault(); 
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1200);
+  });
+});
+var isFlag = true;
+
+var menu = function menu() {
+  var menu = document.querySelectorAll('#menu')[0];
+  var body = document.querySelectorAll('body')[0];
+
+  if (isFlag) {
+    body.classList.add('hidden');
+    menu.classList.add('menuUp');
+    menu.classList.remove('menuDown');
+    isFlag = false;
+  } else {
+    body.classList.remove('hidden');
+    menu.classList.add('menuDown');
+    menu.classList.remove('menuUp');
+    isFlag = true;
+  }
+};
+
+var menuClose = function menuClose() {
+  var menu = document.querySelectorAll('#menu')[0];
+  var body = document.querySelectorAll('body')[0];
+  menu.classList.add('menuDown');
+  body.classList.remove('hidden');
+  menu.classList.remove('menuDown');
+  menu.classList.remove('menuUp');
+  isFlag = !isFlag;
+};
