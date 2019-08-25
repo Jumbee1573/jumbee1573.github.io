8ddd8463349ab3884902ed0c665269b2d354d1a9
Jumbee1573 <stasik1999box@gmail.com>
2019-08-25 20:06:51 +0300

    delete all

--- nyblesoft/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import notes from "./notes";
-import filters from "./filters";
-import filtration from "./filtration";
-
-const rootReducer = combineReducers({ notes, filters, filtration });
-
-export default rootReducer;
